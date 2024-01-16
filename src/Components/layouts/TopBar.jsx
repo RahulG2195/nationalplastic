f58a87bd99b2d0bfce6c4b4b342f65e262431e53
RahulG2195 <rahul@crezvatic.com>
2024-01-16 17:40:16 +0530

    folder structure and home page design

---
+++ src/Components/layouts/TopBar.jsx
@@ -0,0 +1,12 @@
+
+function TopBar() {
+  return (
+      <div className="row p-3 top_nav">
+          <div className="col">
+            <p className=''>50+ National Plastic Stores across India. Come, Visit Us!</p>
+          </div>
+        </div>
+  )
+}
+
+export default TopBar
---
+++ src/Components/sections/Home/TopPick.jsx
@@ -0,0 +1,15 @@
+
+export default function TopPick() {
+  return (
+    <section className="top_pick_sec common_section">
+        <div className="container">
+            <div className="row">
+                <div className="section_header mx-auto">
+                    <h2><span>Top Picks</span> For You</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
