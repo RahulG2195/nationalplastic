46a30013a3ab17d7f8ca7a1681524e4d5280b187
RahulG2195 <rahul@crezvatic.com>
2024-02-02 14:23:56 +0530

    config change for aws

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // output:'export',
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+}
 
+// Reference number, Number of Bedrooms, Number of Bathrooms, Numberof Employees,Number of Owners,Number of Shares - not reflecting in export
 module.exports = nextConfig
