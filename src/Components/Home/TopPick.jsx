c92f0a6e67a3ee2b8001b3faa5d71ceaa01831fc
Siddhesh-Dhamale <webdeveloper.crezvatic@gmail.com>
2024-02-13 08:53:34 +0530

    before merging responsive and main

--- src/Components/Home/TopPick.jsx
+++ src/Components/Home/TopPick.jsx
@@ -1,4 +1,4 @@
-import CatCards from "../CommonComp/catCards"; 
+import CatCards from "../CommonComp/catCards";
 import { Swiper, SwiperSlide } from "swiper/react";
 import {
   Navigation,
@@ -8,8 +8,6 @@ import {
   A11y,
 } from "swiper/modules";
 import Link from "next/link";
-import CatCards from "../CommonComp/catCards";
-
 export default function TopPick() {
   const productArr = [
     {
@@ -33,15 +31,15 @@ export default function TopPick() {
   ];
   return (
     <section className="top_pick_sec common_section">
-        <div className="container" >
+      <div className="container" >
+        <div className="row">
+          <div className="section_header mx-auto text-center" >
+            <h2><span>Top Picks</span> For You</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.</p>
+          </div>
+          <div className="col-12 products_col">
             <div className="row">
-                <div className="section_header mx-auto text-center" >
-                    <h2><span>Top Picks</span> For You</h2>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.</p>
-                </div>
-                <div className="col-12 products_col">
-                  <div className="row">
-                  <Swiper
+              <Swiper
                 className="swipper"
                 style={{ width: "100%", height: "100%" }}
                 modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
@@ -59,40 +57,40 @@ export default function TopPick() {
                 }}
                 breakpoints={{
                   200: {
-                      slidesPerView: 1,
-                      spaceBetween: 20,
+                    slidesPerView: 1,
+                    spaceBetween: 20,
                   },
                   640: {
-                      slidesPerView: 2,
-                      spaceBetween: 20,
+                    slidesPerView: 2,
+                    spaceBetween: 20,
                   },
                   768: {
-                      slidesPerView: 3,
-                      spaceBetween: 40,
+                    slidesPerView: 3,
+                    spaceBetween: 40,
                   },
                   1024: {
-                      slidesPerView: 3,
-                      spaceBetween: 50,
+                    slidesPerView: 3,
+                    spaceBetween: 50,
                   },
-              }}
+                }}
               >
-                  { productArr.map((product) => (
-                    
-                      <div className="col-md-4" >
-                        <SwiperSlide key={product.key} >
-                        <CatCards
-                          image={product.image}
-                          title={product.title}
-                          url={product.url}
-                        />
-                        </SwiperSlide>
-                      </div> 
-                    ))}
-                    </Swiper>
+                {productArr.map((product) => (
+
+                  <div className="col-md-4" >
+                    <SwiperSlide key={product.key} >
+                      <CatCards
+                        image={product.image}
+                        title={product.title}
+                        url={product.url}
+                      />
+                    </SwiperSlide>
                   </div>
-                </div>
+                ))}
+              </Swiper>
             </div>
+          </div>
         </div>
+      </div>
     </section>
   )
 }
