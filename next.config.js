f4b82e3c21973c48154e4cc34f6514589ba025c7
aishu-crezvatic <aishwarya@crezvatic.com>
2024-02-23 17:10:42 +0530

    Register Login and redirect to homepage

--- next.config.js
+++ next.config.js
@@ -8,7 +8,16 @@ const nextConfig = {
         'MYSQL_USER': 'root',
         'MYSQL_PASSWORD': 'W7301@aishwarya#',
     }
+
+    // env: {
+    //     'MYSQL_HOST': '92.204.139.35',
+    //     'MYSQL_PORT': '3306',
+    //     'MYSQL_DATABASE': 'digitallab_nationalplastic_db',
+    //     'MYSQL_USER': 'digitallab_nationalplastic',
+    //     'MYSQL_PASSWORD': 'National@2024',
+    // }
 }
+
 
 // Reference number, Number of Bedrooms, Number of Bathrooms, Numberof Employees,Number of Owners,Number of Shares - not reflecting in export
 module.exports = nextConfig
--- src/Components/ProductsCatlogue/PreChairCards.jsx
+++ src/Components/ProductsCatlogue/PreChairCards.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import PreChairsCard from "../preChairsCard/preChairsCard";
 import './PreChairCard.css'
