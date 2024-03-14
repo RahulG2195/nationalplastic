1b12661d69ed8176e4752964dca6a290d1234d59
unknown <dineshndr02@gmail.com>
2024-03-14 12:47:38 +0530

    Commited PRechaircards2

--- src/Components/ProductsCatlogue/PreChairCards.jsx
+++ src/Components/ProductsCatlogue/PreChairCards.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import PreChairsCard from "../PreChairsCard/PreChairsCard";
+// import PreChairsCard from "../PreChairsCard/PreChairsCard";
 import "./PreChairCards.css";
 import axios from "axios";
 import { DotLoader } from "react-spinners";
