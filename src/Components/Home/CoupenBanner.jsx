e8cd4fe3aef9b36593ad8e7b6a89e71a97b9fc74
Siddhesh-Dhamale <webdeveloper.crezvatic@gmail.com>
2024-03-18 12:39:21 +0530

    changes of home page

--- src/Components/Home/CoupenBanner.jsx
+++ src/Components/Home/CoupenBanner.jsx
@@ -6,131 +6,124 @@ import Link from "next/link";
 function CoupenBanner() {
   return (
     <>
-      <section className="coupen_sec">
-        <div className="container">
-          <div className="row coupen-row">
-            <div className="col-md-1">
+      {/* <section> */}
+        <div className="yellowcpn">
+          <div className="">
+            <div className="position-relative dsktpbnr">
               <Image
-                src="/Assets/images/gift-box.png"
+                src="/Assets/svg/Group 944/Group 944.png"
                 alt="gift image"
                 width={100}
                 height={100}
                 layout="responsive"
                 objectFit="cover"
               />
-            </div>
-            <div className="col-md-4">
-              <div className="extra-isc">
-                <h4>GET EXTRA 20% OFF</h4>
-                <small>On Signing Up</small>
+              <div className="d-flex gap-4 dSup">
+                <div className="">
+                  <p className="coupen_code">
+                    <strong className="diwali">DIWALI20</strong>
+                  </p>
+                </div>
+                <div className="">
+                  <div className="signup_div">
+                    <Link href="/Login">
+                      <button className="signup">Sign Up Now</button>
+                    </Link>
+                    <p className="ltc">As in Life, T&C applied</p>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="col-md-6">
-              <div className="offers">
-                <h5>USE EXCLUSIVE COUPON</h5>
-                <small>Applicable on Multiple Purchases</small>
+            
+            <div className="position-relative mblBnner">
+              <Image
+                src="/Assets/svg/Group 946/Group 946.png"
+                alt="gift image"
+                width={100}
+                height={100}
+                layout="responsive"
+                objectFit="cover"
+              />
+              <div className="d-flex gap-4 dSupMobile">
+                <div className="">
+                  <p className="coupen_code">
+                    <strong className="diwaliM">DIWALI20</strong>
+                  </p>
+                </div>
+                <div className="">
+                  <div className="signup_div">
+                    <Link href="/Login">
+                      <button className="signup " >Sign Up Now</button>
+                    </Link>
+                    <p className="ltcM">As in Life, T&C applied</p>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="col-md-2">
-              <p className="coupen_code">
-                <strong>DIWALI20</strong>
-              </p>
-            </div>
-            <div className="col-md-2">
-              <div className="signup_div">
-                <Link href="/Login">
-                  <button className="signup">Sign Up Now</button>
-                </Link>
-                <p>As in Life, T&C applied</p>
-              </div>
-            </div>
+
           </div>
         </div>
-      </section>
-      <section className="delivery_inst_Sec">
+      {/* </section> */}
+
+      <div className="delivery_inst_Sec">
         <div className="container delivery_div">
-          <div className="row featuresFour">
-            <div className="col-6 col-md-6 col-lg-3 col-xl-3">
-              <div className="divelerycol d-flex align-center" >
-                    <div className="del_img">
-                        <Image
+          <div className="row featuresFour d-flex justify-content-center">
+            <div className="col-6 col-md-4 col-lg-3 col-xl-3">
+              <div className="divelerycol d-flex align-items-center">
+                <div className="del_img">
+                  <Image
                     src="/Assets/images/smiley.png"
                     alt="gift image"
                     width={33}
                     height={33}
-                    />
-                    </div>
+                  />
+                </div>
                 <p>Happy Customers</p>
               </div>
             </div>
-            {/* <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3">
-              <div className="divelerycol d-flex align-center">
-              <div className="del_img">
-                <Image
-                  src="/assets/images/delivery-truck.png"
-                  alt="gift image"
-                  width={33}
-                  height={33}
-                />
-                </div>
-                <p>Free Shipping</p>
-              </div>
-            </div> */}
-            {/* <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3">
-              <div className="divelerycol d-flex align-center"  data-aos="fade-down">
-                    <div className="del_img">
-                        <Image
-                    src="/assets/images/smiley.png"
+            <div className="col-6 col-md-4 col-xs-6 col-lg-3 col-xl-3">
+              <div className="divelerycol d-flex align-items-center">
+                <div className="del_img">
+                  <Image
+                    src="/Assets/images/delivery-truck.png"
                     alt="gift image"
                     width={33}
                     height={33}
-                    />
-                    </div>
-                <p>Happy Customers</p>
-              </div>
-            </div> */}
-            <div className="col-6  col-sm-6 col-md-6 col-lg-3 col-xl-3">
-              <div className="divelerycol d-flex align-center">
-              <div className="del_img">
-                <Image
-                  src="/Assets/images/delivery-truck.png"
-                  alt="gift image"
-                  width={33}
-                  height={33}
-                />
+                  />
                 </div>
                 <p>Free Shipping</p>
               </div>
             </div>
-            <div className="col-6  col-md-6 col-lg-3 col-xl-3">
-              <div className="divelerycol d-flex align-center">
-              <div className="del_img">
-                <Image
-                  src="/Assets/images/settings.png"
-                  alt="gift image"
-                  width={33}
-                  height={33}
-                />
+            <div className="col-6 col-md-4 col-lg-3 col-xl-3">
+              <div className="divelerycol d-flex align-items-center">
+                <div className="del_img">
+                  <Image
+                    src="/Assets/images/settings.png"
+                    alt="gift image"
+                    width={33}
+                    height={33}
+                  />
                 </div>
                 <p>Free Installation</p>
               </div>
             </div>
-            <div className="col-6  col-md-6 col-lg-3 col-xl-3">
-              <div className="divelerycol d-flex align-center">
-              <div className="del_img">
-                <Image
-                  src="/Assets/images/0.png"
-                  alt="gift image"
-                  width={33}
-                  height={33}
-                />
+            <div className="col-6 col-md-4 col-lg-3 col-xl-3">
+              <div className="divelerycol d-flex align-items-center">
+                <div className="del_img">
+                  <Image
+                    src="/Assets/images/0.png"
+                    alt="gift image"
+                    width={33}
+                    height={33}
+                  />
                 </div>
                 <p>No Cost EMIs</p>
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+
     </>
   );
 }
