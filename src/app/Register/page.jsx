629141d4f79e1cbaf52263bb313287bfc595906d
Dincz <dineshndr02@gmail.com>
2024-04-08 10:20:35 +0530

    Mail update after registration.

--- src/app/Register/page.jsx
+++ src/app/Register/page.jsx
@@ -19,6 +19,12 @@ function Register() {
   });
   const [formErrors, setFormErrors] = useState({});
   const [successMessage, setSuccessMessage] = useState("");
+  const mailUpdate = async () => {
+    const response = await axios.post(
+      "http://thatsyourwebsite.com/api/RegisterEmail",
+      formData
+    );
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -53,7 +59,9 @@ function Register() {
     if (Object.keys(errors).length === 0) {
       try {
         // Check if email already exists
-        const { data } = await axios.get(`http://thatsyourwebsite.com/api/Users`);
+        const { data } = await axios.get(
+          `http://thatsyourwebsite.com/api/Users`
+        );
         const existingEmails = data.map((user) => user.Email);
         if (existingEmails.includes(formData.email)) {
           alert("Email already exists!");
@@ -64,6 +72,7 @@ function Register() {
           );
           //console.log("Form submitted:", response);
           // Clear form data on successful submission
+          mailUpdate(formData);
           setFormData({
             firstName: "",
             lastName: "",
