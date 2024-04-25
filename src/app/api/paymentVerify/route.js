83da08746c4e55fd9a2fdd9f32c28a3ad50887ce
Dincz <dineshndr02@gmail.com>
2024-04-25 10:07:53 +0530

    paymentVerify

--- src/app/api/paymentVerify/route.js
+++ src/app/api/paymentVerify/route.js
@@ -1,16 +1,19 @@
-import { NextResponse } from "next/server";
-import Razorpay from "razorpay";
-import shortid from "shortid";
 import crypto from "crypto";
-const instance = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_SECRET,
-});
 
 export async function POST(req, res) {
   try {
     const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
       await req.json();
+
+    // Validation
+    if (!razorpay_order_id || !razorpay_payment_id || !razorpay_signature) {
+      return new Response(
+        JSON.stringify({
+          status: 400,
+          message: "Missing required data in request body",
+        })
+      );
+    }
 
     const body = razorpay_order_id + "|" + razorpay_payment_id;
 
@@ -18,29 +21,36 @@ export async function POST(req, res) {
       .createHmac("sha256", process.env.RAZORPAY_SECRET)
       .update(body.toString())
       .digest("hex");
-    console.log("e--s", expectedSignature);
-    console.log("--", razorpay_signature);
+
+    console.log("Expected Signature:", expectedSignature);
+    console.log("Received Signature:", razorpay_signature);
 
     const isAuthentic = expectedSignature === razorpay_signature;
 
     if (isAuthentic) {
-      return NextResponse.json(
-        {
-          message: "success",
-        },
-        {
+      console.log("Payment verification successful");
+      return new Response(
+        JSON.stringify({
           status: 200,
-        }
+          message: "Success",
+        })
+      );
+    } else {
+      console.log("Payment verification failed");
+      return new Response(
+        JSON.stringify({
+          status: 400,
+          message: "Invalid signature",
+        })
       );
     }
   } catch (err) {
-    return NextResponse.json(
-      {
-        message: err.message,
-      },
-      {
-        status: 501,
-      }
+    console.error("Error verifying payment:", err);
+    return new Response(
+      JSON.stringify({
+        status: 500,
+        message: "Internal Server Error",
+      })
     );
   }
 }
