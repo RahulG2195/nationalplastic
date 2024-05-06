8e8014a7917ca79ec53866587efaf1fcd72f79d0
Dincz <dineshndr02@gmail.com>
2024-05-06 11:19:21 +0530

    id

--- src/app/api/razorpay/route.js
+++ src/app/api/razorpay/route.js
@@ -2,11 +2,6 @@ import { NextResponse } from "next/server";
 import Razorpay from "razorpay";
 import { v4 as uuidv4 } from "uuid"; // Import for generating unique IDs
 
-const instance = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_SECRET,
-});
-
 // export async function GET() {
 //   const payment_capture = 1;
 //   const amount = 1 * 100; // amount in paisa. In our case it's INR 1
@@ -26,6 +21,10 @@ export async function POST(request) {
       isBrowser,
     } = await request.json();
     if (isBrowser) {
+      const instance = new Razorpay({
+        key_id: process.env.RAZORPAY_KEY_ID,
+        key_secret: process.env.RAZORPAY_SECRET,
+      });
       const options = {
         amount: amount,
         currency,
@@ -45,9 +44,10 @@ export async function POST(request) {
   } catch (error) {
     console.error("error ", error);
     return new Response(
-      JSON.stringify({ message: error || "INTERNAL SERVER ERROR" }), {
-      status: 500,
-    }
+      JSON.stringify({ message: error || "INTERNAL SERVER ERROR" }),
+      {
+        status: 500,
+      }
     );
   }
 }
@@ -57,6 +57,10 @@ export async function PUT(request) {
     const { razorpay_payment_id, isBrowser } = await request.json();
 
     if (isBrowser) {
+      const instance = new Razorpay({
+        key_id: process.env.RAZORPAY_KEY_ID,
+        key_secret: process.env.RAZORPAY_SECRET,
+      });
       const response = await instance.payments.fetch(razorpay_payment_id);
       return NextResponse.json({ msg: "successss", response });
     } else {
