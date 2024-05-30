342f1da25577a2793984dd6adb29710f480ca1a8
Rahul Gupta <Rahul@webfarmsolutions.in>
2024-05-30 16:55:11 +0530

    query changes

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
     MYSQL_HOST: "localhost",
     MYSQL_DATABASE: "nationalplastic",
     MYSQL_USER: "root",
-    MYSQL_PASSWORD: "National2024",
+    MYSQL_PASSWORD: "Webfarm2024",
     MYSQL_PORT: "3306",
     RAZORPAY_KEY_ID: "rzp_test_WUEWvbWJ3T7hJ0",
     RAZORPAY_SECRET: "onArQM2aO7a1EUWCV8q7Gjrv",
--- src/app/api/ProductsCat/route.js
+++ src/app/api/ProductsCat/route.js
@@ -7,7 +7,7 @@ export async function GET(request) {
   const queryParams = parsedUrl.query.query;
   try {
     const products = await query({
-      query: "SELECT product_id, product_name, seo_url, category_id, image_name, price, discount_price, discount_percentage, categoryType, duration, InstallationCharges, color, color_code, armType, prod_status FROM products where category_id = ?  AND prod_status = 1 GROUP BY product_name",
+      query: "SELECT MIN(product_id) AS product_id, product_name, MIN(seo_url) AS seo_url, MIN(category_id) AS category_id, MIN(image_name) AS image_name, MIN(price) AS price, MIN(discount_price) AS discount_price, MIN(discount_percentage) AS discount_percentage, MIN(categoryType) AS categoryType, MIN(duration) AS duration, MIN(InstallationCharges) AS InstallationCharges, MIN(color) AS color, MIN(color_code) AS color_code, MIN(armType) AS armType, prod_status FROM products where category_id = ?  AND prod_status = 1 GROUP BY product_name",
       values: [`${queryParams}`],
     });
 
