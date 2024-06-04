5d01475d6caeea52e09a2832fb538d335d888d2a
Rahul Gupta <Rahul@webfarmsolutions.in>
2024-06-04 10:46:37 +0530

    some design fixed

--- src/app/api/colorBasedProduct/route.js
+++ src/app/api/colorBasedProduct/route.js
@@ -3,13 +3,23 @@ import { query } from "@/lib/db";
 
 export async function POST(request) {
   try {
-    const { name, color } = await request.json();
+    const { name, color,product_id } = await request.json();
+    let getProducts;
+    if(product_id) {
 
-    const getProducts = await query({
-      query: "SELECT * FROM products WHERE product_name = ? AND color = ?",
+       getProducts = await query({
+        query: "SELECT * FROM products WHERE product_id = ?",
+        values: [product_id],
+      });
+
+    }else{
+
+     getProducts = await query({
+      query: "SELECT * FROM products WHERE seo_url = ? AND color = ?",
       values: [name, color],
     });
-
+  }
+  console.log("getproducts", getProducts);
     return new Response(
       JSON.stringify({
         status: 200,
