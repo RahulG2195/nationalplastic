005e4738ce5d571d7837955e437d323558bb7f5d
RahulG2195 <rahul@crezvatic.com>
2024-07-15 16:28:38 +0530

    favicon cache

--- next.config.js
+++ next.config.js
@@ -3,14 +3,20 @@
 const nextConfig = {
   //   reactStrictMode: false,
   // output:'export',
-    // runtime: 'nodejs',
+  // runtime: 'nodejs',
   eslint: {
     ignoreDuringBuilds: true,
   },
   // // images:{
   //     unoptimized:true,
-// },
-
+  // },
+  source: "/favicon.ico",
+  headers: [
+    {
+      key: "Cache-Control",
+      value: "public, max-age=86400, must-revalidate",
+    },
+  ],
 };
 
 // Reference number, Number of Bedrooms, Number of Bathrooms, Numberof Employees,Number of Owners,Number of Shares - not reflecting in export
