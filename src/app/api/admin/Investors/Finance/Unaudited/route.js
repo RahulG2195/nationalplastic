eaaf78b4046cabc92ed79be6336c5b90e055de84
Rahul Gupta <Rahul@webfarmsolutions.in>
2024-08-10 15:04:50 +0530

    code for update

--- src/app/api/admin/Investors/Finance/Unaudited/route.js
+++ src/app/api/admin/Investors/Finance/Unaudited/route.js
@@ -8,9 +8,6 @@ const path = require("path");
 
 const uploadPDF = async (file) => {
   try {
-    console.log("Starting PDF upload process");
-    console.log("Received file object:", file);
-    
     if (!file || typeof file.arrayBuffer !== "function") {
       console.error("Invalid file object received");
       throw new Error("Invalid file object");
@@ -19,25 +16,17 @@ const uploadPDF = async (file) => {
     const bytes = await file.arrayBuffer();
     const buffer = Buffer.from(bytes);
     
-    console.log("NEXT_PUBLIC_EXTERNAL_PATH_DIR:", process.env.NEXT_PUBLIC_EXTERNAL_PATH_DIR);
-    console.log("NEXT_PUBLIC_PDF_PATH_DIR:", process.env.NEXT_PUBLIC_INVESTORS_PATH_DIR);
-    
     const uploadDir = `${process.env.NEXT_PUBLIC_EXTERNAL_PATH_DIR}${process.env.NEXT_PUBLIC_INVESTORS_PATH_DIR}`;
-    console.log("Upload directory:", uploadDir);
 
     try {
       await fs.access(uploadDir);
-      console.log("Upload directory exists");
     } catch {
-      console.log("Creating upload directory");
       await fs.mkdir(uploadDir, { recursive: true });
     }
 
     const filePath = path.join(uploadDir, file.name);
-    console.log("File will be saved to:", filePath);
     
     await fs.writeFile(filePath, buffer);
-    console.log(`File successfully uploaded to ${filePath}`);
     
     return file.name;
   } catch (error) {
@@ -53,19 +42,14 @@ export async function POST(request) {
   const quarter = formData.get('quarter');
   const file_name = formData.get('file_name');
 
-  console.log("Received POST request");
-
     if (!file_name) {
-      console.error("No file provided in form data");
       return NextResponse.json({ message: "No file provided" }, { status: 400 });
     }
 
-    console.log("File received:", file_name.name);
 
     const fileName = await uploadPDF(file_name);
     // const pdfPath = `${process.env.NEXT_PUBLIC_PDF_PATH_DIR}/${fileName}`;
 
-    console.log("PDF uploaded successfully. Path:", fileName);
   // const pdfPath = fileName;
 
   try {
@@ -89,17 +73,12 @@ export async function PUT(request) {
   const quarter = formData.get('quarter');
   const file_name = formData.get('file_name');
 
-  let pdfPath = '';
 
-  if (file_name) {
-    try {
-      await uploadFile(file_name);
-      pdfPath = `${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_INVESTORS_PATH_DIR}${file_name.name}`;
-    } catch (error) {
-      console.error('file_name upload error:', error);
-      return NextResponse.json({ message: "Error saving file_name" }, { status: 500 });
-    }
+  if (!file_name) {
+    return NextResponse.json({ message: "No file provided" }, { status: 400 });
   }
+  
+  const fileName = await uploadPDF(file_name);
 
   try {
     let updateQuery = `
