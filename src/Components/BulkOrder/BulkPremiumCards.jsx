91820f59d53e69ffbd2ab6cdae63c2853144b308
Shankar Panaskar <shankar.crezvatic@gmail.com>
2024-08-13 14:43:06 +0530

    bulk order cards updated

--- src/Components/BulkOrder/BulkPremiumCards.jsx
+++ src/Components/BulkOrder/BulkPremiumCards.jsx
@@ -1,9 +1,11 @@
 import PreChairsCard from "@/Components/preChairsCard/preChairsCard.jsx";
+import { Bounce, toast } from "react-toastify";
 
-const BulkPremiumCards = ({proddata}) => {
+const BulkPremiumCards = ({ proddata }) => {
   const chairData = [
     {
-      ChairImg: "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
+      ChairImg:
+        "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
       Title: "SHAMIYANA",
       Discription: "Lorem ipsum dolor sit amet.",
       Price: "00,000",
@@ -11,7 +13,8 @@ const BulkPremiumCards = ({proddata}) => {
       Discount: "20%",
     },
     {
-      ChairImg: "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
+      ChairImg:
+        "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
       Title: "SHAMIYANA",
       Discription: "Lorem ipsum dolor sit amet.",
       Price: "00,000",
@@ -19,7 +22,8 @@ const BulkPremiumCards = ({proddata}) => {
       Discount: "20%",
     },
     {
-      ChairImg: "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
+      ChairImg:
+        "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
       Title: "SHAMIYANA",
       Discription: "Lorem ipsum dolor sit amet.",
       Price: "00,000",
@@ -27,7 +31,8 @@ const BulkPremiumCards = ({proddata}) => {
       Discount: "20%",
     },
     {
-      ChairImg: "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
+      ChairImg:
+        "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
       Title: "SHAMIYANA",
       Discription: "Lorem ipsum dolor sit amet.",
       Price: "00,000",
@@ -35,7 +40,8 @@ const BulkPremiumCards = ({proddata}) => {
       Discount: "20%",
     },
     {
-      ChairImg: "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
+      ChairImg:
+        "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
       Title: "SHAMIYANA",
       Discription: "Lorem ipsum dolor sit amet.",
       Price: "00,000",
@@ -43,7 +49,8 @@ const BulkPremiumCards = ({proddata}) => {
       Discount: "20%",
     },
     {
-      ChairImg: "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
+      ChairImg:
+        "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
       Title: "SHAMIYANA",
       Discription: "Lorem ipsum dolor sit amet.",
       Price: "00,000",
@@ -51,7 +58,8 @@ const BulkPremiumCards = ({proddata}) => {
       Discount: "20%",
     },
     {
-      ChairImg: "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
+      ChairImg:
+        "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
       Title: "SHAMIYANA",
       Discription: "Lorem ipsum dolor sit amet.",
       Price: "00,000",
@@ -59,7 +67,8 @@ const BulkPremiumCards = ({proddata}) => {
       Discount: "20%",
     },
     {
-      ChairImg: "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
+      ChairImg:
+        "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
       Title: "SHAMIYANA",
       Discription: "Lorem ipsum dolor sit amet.",
       Price: "00,000",
@@ -67,7 +76,8 @@ const BulkPremiumCards = ({proddata}) => {
       Discount: "20%",
     },
     {
-      ChairImg: "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
+      ChairImg:
+        "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
       Title: "SHAMIYANA",
       Discription: "Lorem ipsum dolor sit amet.",
       Price: "00,000",
@@ -75,7 +85,8 @@ const BulkPremiumCards = ({proddata}) => {
       Discount: "20%",
     },
     {
-      ChairImg: "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
+      ChairImg:
+        "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
       Title: "SHAMIYANA",
       Discription: "Lorem ipsum dolor sit amet.",
       Price: "00,000",
@@ -83,7 +94,8 @@ const BulkPremiumCards = ({proddata}) => {
       Discount: "20%",
     },
     {
-      ChairImg: "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
+      ChairImg:
+        "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
       Title: "SHAMIYANA",
       Discription: "Lorem ipsum dolor sit amet.",
       Price: "00,000",
@@ -91,7 +103,8 @@ const BulkPremiumCards = ({proddata}) => {
       Discount: "20%",
     },
     {
-      ChairImg: "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
+      ChairImg:
+        "${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}products.png",
       Title: "SHAMIYANA",
       Discription: "Lorem ipsum dolor sit amet.",
       Price: "00,000",
@@ -101,25 +114,75 @@ const BulkPremiumCards = ({proddata}) => {
   ];
   const Np = "National Plastic";
 
+  const handleAddToWishlist = async (
+    product_id,
+  
+  ) => {
+    try {
+      const isLoggedInResult = await isLoggedIn();
+      if (!isLoggedInResult) {
+        notifyError();
+        router.push("/Login");
+      } else {
+        dispatch(
+          addItemToWishlist({
+            product_id: product_id,
+          })
+        );
+        notify();
+        fetchWishlistItems();
+      }
+    } catch (error) {
+      notifyinfo();
+      console.error("Error:", error);
+    }
+  };
+  const notifyinfo = () => {
+    toast.success("Already in WISHLIST", {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+    });
+  };
+
+
+
   return (
     <>
       <div className="container mt-5">
         <div className="row ">
-          {proddata.map(chair => {
-            const images = chair.image_name ? chair.image_name.split(', ').map(image => image.trim()) : [];
-            return <div
-              key={chair.product_id}
-              className="PreCardSm col-12 col-sm-6 col-xs-4 col-md-6 col-lg-3 newProdCard"
-            >
-              <PreChairsCard
-                id={chair.product_id}
-                ChairImg={`${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}${images[0]}`}
-                Title={`${chair.product_name} (${chair.color})`}
-                Discription={chair.short_description}
-                Discount={chair.discount_percentage}
-                showGetQuote={true}
-              />
-            </div>
+          {proddata.map((chair) => {
+            const images = chair.image_name
+              ? chair.image_name.split(", ").map((image) => image.trim())
+              : [];
+            return (
+              <div
+                key={chair.product_id}
+                className="PreCardSm col-12 col-sm-6 col-xs-4 col-md-6 col-lg-3 newProdCard"
+              >
+                <PreChairsCard
+                  id={chair.product_id}
+                  ChairImg={`${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_PRODUCTS_PATH_DIR}${images[0]}`}
+                  Title={`${chair.product_name}`}
+                  Color={`${chair.color}`}
+                  Discription={chair.short_description}
+                  Discount={chair.discount_percentage}
+                  showGetQuote={true}
+                  onaddToWishlist={() =>
+                    handleAddToWishlist(
+                      chair.product_id,
+        
+                    )
+                  }
+                />
+              </div>
+            );
           })}
         </div>
       </div>
