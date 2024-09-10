a3417005bfb15d6b119aa21911ec77caf2a5503b
RahulG2195 <rahul@crezvatic.com>
2024-09-10 09:58:57 +0530

    debug cache issue

--- src/app/api/NavCategory/route.js
+++ src/app/api/NavCategory/route.js
@@ -1,26 +1,31 @@
 import { query } from "@/lib/db";
+import { console } from 'console';
+
 
 export async function GET(request) {
   try {
-    const Categories = await query({
-      //ORDER BY header_position ASC
-      query:
-        `SELECT category_id, seo_url, category_name, navshow, image_name, header_position, 
-       UNIX_TIMESTAMP(added_on) as last_updated FROM categories WHERE navshow = 1 AND status = 1 ORDER BY header_position`,
+    const categories = await query({
+      query: `
+        SELECT category_id, seo_url, category_name, navshow, image_name, header_position, 
+               UNIX_TIMESTAMP(added_on) as last_updated
+        FROM categories
+        WHERE navshow = 1 AND status = 1
+        ORDER BY header_position
+      `,
       values: [],
     });
 
     return new Response(
       JSON.stringify({
         status: 200,
-        navshow: Categories,
+        navshow: categories,
       })
     );
   } catch (error) {
     return new Response(
       JSON.stringify({
         status: 500,
-        message: error.message
+        message: error.message,
       })
     );
   }
