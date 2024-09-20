576df8ff7fe25040c318b86a029e925549979c80
Dincz <dineshndr02@gmail.com>
2024-09-20 15:18:46 +0530

    Ploicy pages.

--- src/app/Refund/page.jsx
+++ src/app/Refund/page.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Image from "next/image";
 
+
+
 const RefundPolicy = () => {
   const [pageContents, setPageContents] = useState("");
   const [loading, setLoading] = useState(true);
