322967e5703f60d0e903445e3a3942de4af17852
Dincz <dineshndr02@gmail.com>
2024-11-08 14:52:33 +0530

    BottomBar

--- src/Components/layouts/Header.jsx
+++ src/Components/layouts/Header.jsx
@@ -727,7 +727,7 @@ export default function Header() {
                 </div>
               </div>
             </nav>
-            {/* {shouldRenderBottomBar ? <BottomBar /> : <PlaceholderBar />} */}
+            {shouldRenderBottomBar ? <BottomBar /> : <PlaceholderBar />}
 
           </div>
         </>
