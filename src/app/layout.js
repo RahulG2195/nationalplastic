5ddb8fee513686fa9f398064b712e6fd06b65103
Dincz <dineshndr02@gmail.com>
2024-12-04 21:51:44 +0530

    Google tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -51,14 +51,46 @@ export default function RootLayout({ children }) {
           src="https://checkout.razorpay.com/v1/checkout.js"
           strategy="beforeInteractive"
         />
+        <Script
+          id="gtm-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KRPVWJ97');
+          `,
+          }}
+        />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KPGV02X0G8"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-KPGV02X0G8');
+            `,
+          }}
+        />
       </head>
 
       <body className={inter.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KRPVWJ97"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <SessionProvider>
           <BrowserRouter>
             <Provider store={store}>
               <PersistGate loading={null} persistor={persistor}>
-                 {!isAdmin && <Header />}
+                {!isAdmin && <Header />}
                 <div className={`${isAdmin ? "pt-0 mt-0" : "mobile__top"}`}>
                   {children}
 
